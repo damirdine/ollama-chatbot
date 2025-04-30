27fcfac6111e2f9ec573029aa59fcbe9bc7e8be5
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-04-30 10:25:00 +0200

    add timer

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import turtle
+import time
 
+duration = 10 # seconds
+start_time = time.time()
 
-while True:
-    bob = turtle.Turtle()
-    bob.color('blue')
-    bob.shape('turtle')
+bob = turtle.Turtle()
+bob.color('blue')
+bob.shape('turtle')
 
+while time.time() - start_time < duration:
     bob.left(90)
     bob.circle(75)
