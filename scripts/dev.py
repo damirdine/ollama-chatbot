9d32a43dddf3f0e3886a489d6437d896254b3d88
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-05-05 12:24:25 +0200

    create poetry scripts

---
+++ scripts/dev.py
@@ -0,0 +1,6 @@
+# scripts/dev.py
+import uvicorn
+
+
+def main():
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
---
+++ scripts/start.py
@@ -0,0 +1,5 @@
+# scripts/start.py
+import uvicorn
+
+def main():
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000)
