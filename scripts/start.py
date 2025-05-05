e1732b73326d51eb9420e0518fac671c848ff1ad
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2025-05-05 22:30:39 +0200

    clean start script

--- scripts/start.py
+++ scripts/start.py
@@ -2,14 +2,7 @@
 import os
 import uvicorn
 
-from dotenv import DotEnv
-dotenv = DotEnv()
-
-if os.path.exists(".env"):
-    dotenv = DotEnv(".env")
-    PORT = dotenv.get("PORT")
-else:
-    PORT = os.getenv("PORT", 8000)
+PORT = os.getenv("PORT", 8000)
 
 def main():
     uvicorn.run("app.main:app", port=PORT)
